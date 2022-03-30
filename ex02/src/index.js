19670f3b60bfea65d4b6238142019c7de895b4d3
Dedo-unuka <dzanazovko@gmail.com>
2022-03-30 12:04:09 +0200

    Addin ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+// Only change code below this line
+
+// This is a single line JavaScript comment
+/*This is a 
+multi-line
+Javascript comment;
+*/
+
+// Only change code above this line
+
+}
+console.log(main());
+
+module.exports = main;
